a280d23d930a0d13bbccc50a21c6ac48ceb17a08
Pavel Ershov <devnulpavel@gmail.com>
2021-01-17 19:35:45 +0300

    First release (2)

--- Formula/m3u8_downloader.rb
+++ Formula/m3u8_downloader.rb
@@ -1,14 +1,13 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class M3U8_Downloader < Formula
+class M3u8Downloader < Formula
     desc "Download m3u8 hls streams in Rust"
     homepage "https://github.com/DevNulPavel/m3u8_downloader"
     url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/v0.0.1/m3u8_downloader-arm64.tar.gz"
     sha256 "521636690ae2e92a6458b9e82906fb99c04dd609afec6287a72920d54fd8c975"
-    version "1.0.0"
+    version "0.0.1"
   
     def install
-      bin.install "m3u8_downloader"
+        bin.install "m3u8_downloader"
     end
 end
