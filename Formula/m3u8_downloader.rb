2cb45322aea13f17f715572fb46f95a5c355aea9
Pavel Ershov <devnulpavel@gmail.com>
2021-01-17 19:57:35 +0300

    Universal arch build

--- Formula/m3u8_downloader.rb
+++ Formula/m3u8_downloader.rb
@@ -3,9 +3,9 @@
 class M3u8Downloader < Formula
     desc "Download m3u8 hls streams in Rust"
     homepage "https://github.com/DevNulPavel/m3u8_downloader"
-    url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/v0.0.1/m3u8_downloader-arm64.tar.gz"
-    sha256 "521636690ae2e92a6458b9e82906fb99c04dd609afec6287a72920d54fd8c975"
-    version "0.0.1"
+    url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/1.0.0/m3u8_downloader-universal.tar.gz"
+    sha256 "825ac395664ce89b48fd4d2672f551eec8eaa42e44962d979e2995a896695d4a"
+    version "1.0.0"
   
     def install
         bin.install "m3u8_downloader"
