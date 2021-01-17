67ac197ba81691cbcec5477ec7160853de9a8253
Pavel Ershov <devnulpavel@gmail.com>
2021-01-17 20:33:31 +0300

    Small fix

--- Formula/m3u8_downloader.rb
+++ Formula/m3u8_downloader.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class M3u8Downloader < Formula
-    desc "Download m3u8 hls streams in Rust"
+    desc "Download m3u8 HLS streams, written in Rust"
     homepage "https://github.com/DevNulPavel/m3u8_downloader"
     url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/1.0.0/m3u8_downloader-universal.tar.gz"
     sha256 "825ac395664ce89b48fd4d2672f551eec8eaa42e44962d979e2995a896695d4a"
--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+TAP:
+	brew tap DevNulPavel/m3u8_downloader
+
 AUDID:
 	brew audit --strict --online m3u8_downloader
-
